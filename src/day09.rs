b1a078db2a934a2e3dbc24704f97db482fa8b784
Krishna Sundarram <krishnasundarram@monzo.com>
2021-12-27 10:32:58 +0530

    Refactor Matrix and add new APIs for day 11

--- src/day09.rs
+++ src/day09.rs
@@ -1,37 +1,24 @@
 use std::collections::{HashSet, VecDeque};
 
-use crate::matrix::Matrix;
+use crate::matrix::{Matrix, Point};
 
 impl Matrix {
     #[allow(dead_code)]
     fn low_point_value_sum(&self) -> u32 {
         self.all_points()
-            .iter()
-            .filter(|(x, y)| self.is_low_point(*x, *y))
-            .filter_map(|(x, y)| self.value(*x, *y))
+            .filter(|p| self.is_low_point(p))
+            .filter_map(|p| self.value(&p))
             .map(|val| val + 1)
             .sum()
     }
 
-    fn is_low_point(&self, x: usize, y: usize) -> bool {
-        let right = self.is_lower(x, y, x + 1, y);
-        let bottom = self.is_lower(x, y, x, y + 1);
-        let left = if x > 0 {
-            self.is_lower(x, y, x - 1, y)
-        } else {
-            true
-        };
-        let top = if y > 0 {
-            self.is_lower(x, y, x, y - 1)
-        } else {
-            true
-        };
-
-        right && bottom && left && top
+    fn is_low_point(&self, point: &Point) -> bool {
+        self.neighbours(*point)
+            .all(|neighbour| self.is_lower(point, &neighbour))
     }
 
-    fn is_lower(&self, x: usize, y: usize, other_x: usize, other_y: usize) -> bool {
-        match (self.value(other_x, other_y), self.value(x, y)) {
+    fn is_lower(&self, point: &Point, other: &Point) -> bool {
+        match (self.value(other), self.value(point)) {
             (Some(other_val), Some(val)) => other_val > val,
             (_, None) => unreachable!("Element should always be reachable"),
             (None, _) => true,
@@ -44,34 +31,25 @@ impl Matrix {
         let mut q = VecDeque::new();
         let mut result = Vec::new();
 
-        for (x, y) in self.all_points() {
+        for point in self.all_points() {
             let mut basin_size = 0;
-            q.push_back((x, y));
+            q.push_back(point);
             while !q.is_empty() {
-                let (x, y) = match q.pop_front() {
+                let point = match q.pop_front() {
                     Some(point) => point,
                     None => continue,
                 };
 
-                if *self.value(x, y).unwrap() == 9 || visited.contains(&(x, y)) {
-                    visited.insert((x, y));
+                if *self.value(&point).unwrap() == 9 || visited.contains(&point) {
+                    visited.insert(point);
                     continue;
                 }
 
-                visited.insert((x, y));
+                visited.insert(point);
                 basin_size += 1;
 
-                if x + 1 < self.max_x {
-                    q.push_back((x + 1, y));
-                }
-                if y + 1 < self.max_y {
-                    q.push_back((x, y + 1));
-                }
-                if x > 0 {
-                    q.push_back((x - 1, y));
-                }
-                if y > 0 {
-                    q.push_back((x, y - 1));
+                for neighbour in self.neighbours(point) {
+                    q.push_back(neighbour);
                 }
             }
             result.push(basin_size)
--- src/matrix.rs
+++ src/matrix.rs
@@ -4,6 +4,12 @@ pub struct Matrix {
     pub max_y: usize,
 }
 
+#[derive(PartialEq, Eq, Clone, Copy, Debug, Hash)]
+pub struct Point {
+    pub x: usize,
+    pub y: usize,
+}
+
 impl Matrix {
     #[allow(dead_code)]
     pub fn new(input: &str) -> Option<Matrix> {
@@ -25,13 +31,187 @@ impl Matrix {
         })
     }
 
-    pub fn all_points(&self) -> Vec<(usize, usize)> {
+    pub fn all_points(&self) -> impl Iterator<Item = Point> + '_ {
         (0..self.max_x)
             .flat_map(|x| std::iter::repeat(x).zip(0..self.max_y))
-            .collect()
+            .map(|(x, y)| Point { x, y })
     }
 
-    pub fn value(&self, x: usize, y: usize) -> Option<&u32> {
-        self.storage.get(y * self.max_x + x)
+    pub fn find(&self, needle: u32) -> impl Iterator<Item = Point> + '_ {
+        self.all_points()
+            .filter(move |point| match self.value(point) {
+                Some(val) => *val == needle,
+                None => false,
+            })
+    }
+
+    pub fn value(&self, point: &Point) -> Option<&u32> {
+        self.storage.get(point.y * self.max_x + point.x)
+    }
+
+    pub fn set(&mut self, point: &Point, val: u32) {
+        self.storage[(point.y * self.max_x + point.x)] = val;
+    }
+
+    pub fn add_to_all(&mut self, n: u32) {
+        for x in self.storage.iter_mut() {
+            *x += n;
+        }
+    }
+
+    pub fn add(&mut self, point: &Point, n: u32) {
+        self.storage[(point.y * self.max_x + point.x)] += n;
+    }
+
+    pub fn neighbours(&self, point: Point) -> impl Iterator<Item = Point> + '_ {
+        let x = point.x as i32;
+        let y = point.y as i32;
+        [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]
+            .into_iter()
+            .filter(|(x, y)| *x >= 0 && *y >= 0)
+            .map(|(x, y)| (x as usize, y as usize))
+            .filter(|(x, y)| *x < self.max_x && *y < self.max_y)
+            .map(|(x, y)| Point { x, y })
+    }
+
+    pub fn neighbours_with_diagonals(&self, point: Point) -> impl Iterator<Item = Point> + '_ {
+        let x = point.x as i32;
+        let y = point.y as i32;
+        (x - 1..=x + 1)
+            .flat_map(move |x| std::iter::repeat(x).zip(y - 1..=y + 1))
+            .filter(|(x, y)| *x >= 0 && *y >= 0)
+            .map(|(x, y)| (x as usize, y as usize))
+            .filter(|(x, y)| *x < self.max_x && *y < self.max_y)
+            .map(|(x, y)| Point { x, y })
+            .filter(move |p| point != *p)
+    }
+
+    pub fn len(&self) -> usize {
+        self.storage.len()
+    }
+}
+
+impl std::fmt::Display for Matrix {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for (i, x) in self.storage.iter().enumerate() {
+            if i % self.max_x == 0 && i != 0 {
+                f.write_str("\n")?;
+            }
+            f.write_fmt(format_args!("{}\t", x))?;
+        }
+        f.write_str("\n")?;
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::collections::HashSet;
+
+    use anyhow::{anyhow, Result};
+
+    use super::{Matrix, Point};
+
+    #[test]
+    fn test_add_to_all() -> Result<()> {
+        let input = "00\n00";
+        let mut matrix = Matrix::new(&input).ok_or(anyhow!("Could not construct matrix"))?;
+        matrix.add_to_all(1);
+        let sum: u32 = matrix.storage.iter().sum();
+        assert_eq!(sum, 4);
+        Ok(())
+    }
+
+    #[test]
+    fn test_find() -> Result<()> {
+        let input = "01\n22";
+        let matrix = Matrix::new(&input).ok_or(anyhow!("Could not construct matrix"))?;
+        let mut iterator = matrix.find(0);
+        assert_eq!(iterator.next(), Some(Point { x: 0, y: 0 }));
+        assert_eq!(iterator.next(), None);
+
+        let mut iterator = matrix.find(1);
+        assert_eq!(iterator.next(), Some(Point { x: 1, y: 0 }));
+        assert_eq!(iterator.next(), None);
+
+        let mut iterator = matrix.find(2);
+        assert_eq!(iterator.next(), Some(Point { x: 0, y: 1 }));
+        assert_eq!(iterator.next(), Some(Point { x: 1, y: 1 }));
+        assert_eq!(iterator.next(), None);
+
+        let mut iterator = matrix.find(3);
+        assert_eq!(iterator.next(), None);
+        Ok(())
+    }
+
+    #[test]
+    fn test_neighbours() -> Result<()> {
+        let input = "012\n345\n678";
+        let matrix = Matrix::new(&input).ok_or(anyhow!("Could not construct matrix"))?;
+
+        let neighbours = matrix
+            .neighbours(Point { x: 0, y: 0 })
+            .collect::<HashSet<Point>>();
+        assert!(neighbours.contains(&Point { x: 1, y: 0 }));
+        assert!(neighbours.contains(&Point { x: 0, y: 1 }));
+        assert_eq!(neighbours.len(), 2);
+
+        let neighbours = matrix
+            .neighbours(Point { x: 1, y: 0 })
+            .collect::<HashSet<Point>>();
+        assert!(neighbours.contains(&Point { x: 0, y: 0 }));
+        assert!(neighbours.contains(&Point { x: 2, y: 0 }));
+        assert!(neighbours.contains(&Point { x: 1, y: 1 }));
+        assert_eq!(neighbours.len(), 3);
+
+        let neighbours = matrix
+            .neighbours(Point { x: 1, y: 1 })
+            .collect::<HashSet<Point>>();
+        assert!(neighbours.contains(&Point { x: 1, y: 0 }));
+        assert!(neighbours.contains(&Point { x: 0, y: 1 }));
+        assert!(neighbours.contains(&Point { x: 2, y: 1 }));
+        assert!(neighbours.contains(&Point { x: 1, y: 2 }));
+        assert_eq!(neighbours.len(), 4);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_neighbours_with_diagonals() -> Result<()> {
+        let input = "012\n345\n678";
+        let matrix = Matrix::new(&input).ok_or(anyhow!("Could not construct matrix"))?;
+
+        let neighbours = matrix
+            .neighbours_with_diagonals(Point { x: 0, y: 0 })
+            .collect::<HashSet<Point>>();
+        assert!(neighbours.contains(&Point { x: 1, y: 0 }));
+        assert!(neighbours.contains(&Point { x: 0, y: 1 }));
+        assert!(neighbours.contains(&Point { x: 1, y: 1 }));
+        assert_eq!(neighbours.len(), 3);
+
+        let neighbours = matrix
+            .neighbours_with_diagonals(Point { x: 1, y: 0 })
+            .collect::<HashSet<Point>>();
+        assert!(neighbours.contains(&Point { x: 0, y: 0 }));
+        assert!(neighbours.contains(&Point { x: 2, y: 0 }));
+        assert!(neighbours.contains(&Point { x: 1, y: 1 }));
+        assert!(neighbours.contains(&Point { x: 0, y: 1 }));
+        assert!(neighbours.contains(&Point { x: 2, y: 1 }));
+        assert_eq!(neighbours.len(), 5);
+
+        let neighbours = matrix
+            .neighbours_with_diagonals(Point { x: 1, y: 1 })
+            .collect::<HashSet<Point>>();
+        assert!(neighbours.contains(&Point { x: 0, y: 0 }));
+        assert!(neighbours.contains(&Point { x: 0, y: 1 }));
+        assert!(neighbours.contains(&Point { x: 0, y: 2 }));
+        assert!(neighbours.contains(&Point { x: 1, y: 0 }));
+        assert!(neighbours.contains(&Point { x: 1, y: 2 }));
+        assert!(neighbours.contains(&Point { x: 2, y: 0 }));
+        assert!(neighbours.contains(&Point { x: 2, y: 1 }));
+        assert!(neighbours.contains(&Point { x: 2, y: 2 }));
+        assert_eq!(neighbours.len(), 8);
+
+        Ok(())
     }
 }
