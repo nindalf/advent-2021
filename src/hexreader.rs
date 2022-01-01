9354ce3455a79d93ad7cfd0107bba1c0597e3297
Krishna Sundarram <krishnasundarram@monzo.com>
2022-01-01 14:51:00 +0530

    Rewrite HexReader and Packet

--- src/hexreader.rs
+++ src/hexreader.rs
@@ -1,54 +1,82 @@
+use anyhow::{anyhow, Result};
 pub trait BitReader {
-    fn read_bits(&mut self, n: usize) -> Option<u8>;
+    fn read_bool(&mut self) -> Result<bool>;
+    fn read_bits(&mut self, n: usize) -> Result<u16>;
 }
 
-struct HexReader {
+pub struct HexReader {
     bytes: Vec<u8>,
     byte_offset: usize,
     bit_offset: usize,
 }
 
 impl BitReader for HexReader {
-    // Got this right first try.
     #[allow(dead_code)]
-    fn read_bits(&mut self, n: usize) -> Option<u8> {
-        // Only up to 8 bits supported
-        if n > 8 || n == 0 {
-            return None;
+    fn read_bool(&mut self) -> Result<bool> {
+        Ok(self.read_bits(1)? == 1)
+    }
+
+    #[allow(dead_code)]
+    fn read_bits(&mut self, n: usize) -> Result<u16> {
+        // Only up to 16 bits supported
+        if n > 16 || n == 0 {
+            return Err(anyhow!("Unsupported length"));
         }
         // Nothing left to read
         if self.byte_offset >= self.bytes.len() {
-            return None;
+            return Err(anyhow!("Reader empty"));
         }
-        let mut byte = *self.bytes.get(self.byte_offset)?;
-        if n <= 8 - self.bit_offset {
-            // clear bit_offset bits
-            byte <<= self.bit_offset;
-            // select n bits
-            byte >>= 8 - n;
 
+        /*
+        1. read n bits with offset in current byte
+        2. read last n-x bits in current byte and first x bytes in the next byte. Join
+        3. read n-x bits, 8 bits and x bits. Join.
+        */
+        if n <= 8 - self.bit_offset {
+            let word = self.read_n_bits_with_offset(self.byte_offset, n, self.bit_offset)?;
             self.byte_offset += if n + self.bit_offset == 8 { 1 } else { 0 };
             self.bit_offset = (self.bit_offset + n) % 8;
-        } else {
-            if self.byte_offset + 1 >= self.bytes.len() {
-                return None;
-            }
-            let first_len = 8 - self.bit_offset;
-            let second_len = n - first_len;
-            let second_byte = *self.bytes.get(self.byte_offset + 1)?;
-            byte = ((byte << self.bit_offset) >> self.bit_offset) << second_len;
-            byte |= second_byte >> (8 - second_len);
-
-            self.byte_offset += 1;
-            self.bit_offset = (self.bit_offset + n) % 8;
+            return Ok(word);
         }
-        Some(byte)
+
+        if self.byte_offset + 1 >= self.bytes.len() {
+            return Err(anyhow!("Insufficient bits remaining"));
+        }
+
+        if n <= 16 - self.bit_offset {
+            let first_byte_len = 8 - self.bit_offset;
+            let second_byte_len = n - (8 - self.bit_offset);
+
+            let first = self.read_last_n_bits(self.byte_offset, first_byte_len)? << second_byte_len;
+            let second = self.read_first_n_bits(self.byte_offset + 1, second_byte_len)?;
+
+            self.byte_offset += if n + self.bit_offset == 16 { 2 } else { 1 };
+            self.bit_offset = (self.bit_offset + n) % 8;
+            return Ok(first | second);
+        }
+
+        if self.byte_offset + 2 >= self.bytes.len() {
+            return Err(anyhow!("Insufficient bits remaining"));
+        }
+
+        let first_byte_len = 8 - self.bit_offset;
+        let second_byte_len = 8;
+        let third_byte_len = n - (16 - self.bit_offset);
+        let first = self.read_last_n_bits(self.byte_offset, first_byte_len)?
+            << (second_byte_len + third_byte_len);
+        let second =
+            self.read_first_n_bits(self.byte_offset + 1, second_byte_len)? << third_byte_len;
+        let third = self.read_first_n_bits(self.byte_offset + 2, third_byte_len)?;
+
+        self.byte_offset += 2;
+        self.bit_offset = (self.bit_offset + n) % 8;
+        Ok(first | second | third)
     }
 }
 
 impl HexReader {
     #[allow(dead_code)]
-    fn new(input: &str) -> HexReader {
+    pub fn new(input: &str) -> HexReader {
         let nibbles = input
             .chars()
             .map(Self::map_char_to_bits)
@@ -105,6 +133,47 @@ impl HexReader {
         }
         result
     }
+
+    fn get_byte_at_offset(&self, offset: usize) -> Result<u8> {
+        self.bytes
+            .get(offset)
+            .copied()
+            .ok_or(anyhow!("Out of bounds access"))
+    }
+
+    fn read_last_n_bits(&self, byte_offset: usize, n: usize) -> Result<u16> {
+        if n == 0 {
+            return Ok(0);
+        }
+        let mut byte = self.get_byte_at_offset(byte_offset)?;
+        byte <<= 8 - n;
+        byte >>= 8 - n;
+        Ok(byte as u16)
+    }
+
+    fn read_n_bits_with_offset(
+        &self,
+        byte_offset: usize,
+        n: usize,
+        bit_offset: usize,
+    ) -> Result<u16> {
+        if n == 0 {
+            return Ok(0);
+        }
+        let mut byte = self.get_byte_at_offset(byte_offset)?;
+        byte <<= bit_offset;
+        byte >>= 8 - n;
+        Ok(byte as u16)
+    }
+
+    fn read_first_n_bits(&self, byte_offset: usize, n: usize) -> Result<u16> {
+        if n == 0 {
+            return Ok(0);
+        }
+        let mut byte = self.get_byte_at_offset(byte_offset)?;
+        byte >>= 8 - n;
+        Ok(byte as u16)
+    }
 }
 
 #[cfg(test)]
@@ -133,7 +202,7 @@ mod test {
     }
 
     #[test]
-    fn test_read_bits() {
+    fn test_read_few_bits() {
         let mut reader = HexReader::new("D2FE28");
         assert_eq!(reader.bytes, vec![0b11010010, 0b11111110, 0b00101000]);
         assert_eq!(reader.read_bits(3).unwrap(), 0b110);
@@ -141,22 +210,76 @@ mod test {
         assert_eq!(reader.read_bits(3).unwrap(), 0b101);
         assert_eq!(reader.read_bits(8).unwrap(), 0b11111100);
         assert_eq!(reader.read_bits(6).unwrap(), 0b010100);
-        assert_eq!(reader.read_bits(2), None);
+        assert!(reader.read_bits(2).is_err());
         assert_eq!(reader.read_bits(1).unwrap(), 0b0);
-        assert_eq!(reader.read_bits(1), None);
+        assert!(reader.read_bits(1).is_err());
+    }
+
+    #[test]
+    fn test_read_many_bits() {
+        let mut reader = HexReader::new("D2FE28D2FE28");
+        assert_eq!(
+            reader.bytes,
+            vec![0b11010010, 0b11111110, 0b00101000, 0b11010010, 0b11111110, 0b00101000]
+        );
+        assert_eq!(reader.read_bits(10).unwrap(), 0b1101001011);
+        assert_eq!(reader.read_bits(14).unwrap(), 0b11111000101000);
+        assert_eq!(reader.read_bits(3).unwrap(), 0b110);
+        assert_eq!(reader.read_bits(15).unwrap(), 0b100101111111000);
+        assert_eq!(reader.read_bits(6).unwrap(), 0b101000);
+    }
+
+    #[test]
+    fn test_read_last_n_bits() {
+        let reader = HexReader::new("D2");
+        assert_eq!(reader.bytes, vec![0b11010010]);
+        assert_eq!(reader.read_last_n_bits(0, 0).unwrap(), 0b0);
+        assert_eq!(reader.read_last_n_bits(0, 1).unwrap(), 0b0);
+        assert_eq!(reader.read_last_n_bits(0, 4).unwrap(), 0b0010);
+        assert_eq!(reader.read_last_n_bits(0, 5).unwrap(), 0b10010);
+        assert_eq!(reader.read_last_n_bits(0, 7).unwrap(), 0b1010010);
+        assert_eq!(reader.read_last_n_bits(0, 8).unwrap(), 0b11010010);
+    }
+
+    #[test]
+    fn test_read_first_n_bits() {
+        let reader = HexReader::new("D2");
+        assert_eq!(reader.bytes, vec![0b11010010]);
+        assert_eq!(reader.read_first_n_bits(0, 0).unwrap(), 0b0);
+        assert_eq!(reader.read_first_n_bits(0, 1).unwrap(), 0b1);
+        assert_eq!(reader.read_first_n_bits(0, 4).unwrap(), 0b1101);
+        assert_eq!(reader.read_first_n_bits(0, 5).unwrap(), 0b11010);
+        assert_eq!(reader.read_first_n_bits(0, 7).unwrap(), 0b1101001);
+        assert_eq!(reader.read_first_n_bits(0, 8).unwrap(), 0b11010010);
+    }
+
+    #[test]
+    fn test_read_n_bits_with_offset() {
+        let reader = HexReader::new("D2");
+        assert_eq!(reader.bytes, vec![0b11010010]);
+        assert_eq!(reader.read_n_bits_with_offset(0, 0, 0).unwrap(), 0b0);
+        assert_eq!(reader.read_n_bits_with_offset(0, 0, 4).unwrap(), 0b0);
+        assert_eq!(reader.read_n_bits_with_offset(0, 1, 1).unwrap(), 0b1);
+        assert_eq!(reader.read_n_bits_with_offset(0, 2, 1).unwrap(), 0b10);
+        assert_eq!(reader.read_n_bits_with_offset(0, 4, 0).unwrap(), 0b1101);
+        assert_eq!(reader.read_n_bits_with_offset(0, 4, 3).unwrap(), 0b1001);
+        assert_eq!(reader.read_n_bits_with_offset(0, 5, 0).unwrap(), 0b11010);
+        assert_eq!(reader.read_n_bits_with_offset(0, 5, 2).unwrap(), 0b01001);
+        assert_eq!(reader.read_n_bits_with_offset(0, 7, 0).unwrap(), 0b1101001);
+        assert_eq!(reader.read_n_bits_with_offset(0, 7, 1).unwrap(), 0b1010010);
+        assert_eq!(reader.read_n_bits_with_offset(0, 8, 0).unwrap(), 0b11010010);
     }
 
     #[test]
     fn test_read_unsupported_lengths() {
         let mut reader = HexReader::new("D2FE28");
         assert_eq!(reader.bytes, vec![0b11010010, 0b11111110, 0b00101000]);
-        assert_eq!(reader.read_bits(0), None);
-        assert_eq!(reader.read_bits(9), None);
+        assert!(reader.read_bits(0).is_err());
+        assert!(reader.read_bits(17).is_err());
 
         // Unsupported read doesn't affect reader
         assert_eq!(reader.read_bits(8).unwrap(), 0b11010010);
-        assert_eq!(reader.read_bits(8).unwrap(), 0b11111110);
-        assert_eq!(reader.read_bits(8).unwrap(), 0b00101000);
-        assert_eq!(reader.read_bits(8), None);
+        assert_eq!(reader.read_bits(16).unwrap(), 0b1111111000101000);
+        assert!(reader.read_bits(8).is_err());
     }
 }
--- src/packet.rs
+++ src/packet.rs
@@ -1,49 +1,346 @@
 use crate::hexreader::BitReader;
 
+use anyhow::Result;
+
 pub trait ReadFrom<T: BitReader> {
-    fn read_from(reader: &mut T) -> Self;
+    fn read_from(reader: &mut T) -> Result<Self>
+    where
+        Self: Sized;
 }
 
+#[derive(Debug, PartialEq, Eq)]
+pub struct Packet {
+    header: PacketHeader,
+    content: PacketContent,
+}
+
+impl<T: BitReader> ReadFrom<T> for Packet {
+    fn read_from(reader: &mut T) -> Result<Self> {
+        let header = PacketHeader::read_from(reader)?;
+        let content = PacketContent::read_from(&header.packet_type, reader)?;
+
+        Ok(Packet { header, content })
+    }
+}
+
+impl Packet {
+    fn len(&self) -> usize {
+        PacketHeader::len() + self.content.len()
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
 pub struct PacketHeader {
     pub version: Version,
     pub packet_type: PacketType,
 }
 
 impl<T: BitReader> ReadFrom<T> for PacketHeader {
-    fn read_from(reader: &mut T) -> Self {
-        let version = Version::read_from(reader);
-        let packet_type = PacketType::read_from(reader);
-        PacketHeader {
+    fn read_from(reader: &mut T) -> Result<Self> {
+        let version = Version::read_from(reader)?;
+        let packet_type = PacketType::read_from(reader)?;
+        Ok(PacketHeader {
             version,
             packet_type,
-        }
+        })
     }
 }
 
-pub struct Version(u8);
+impl PacketHeader {
+    fn len() -> usize {
+        Version::len() + PacketType::len()
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct Version(u16);
 
 impl<T: BitReader> ReadFrom<T> for Version {
-    fn read_from(reader: &mut T) -> Self {
-        match reader.read_bits(3) {
-            Some(version) => Version(version),
-            None => panic!("Corrupt packet"),
-        }
+    fn read_from(reader: &mut T) -> Result<Self> {
+        Ok(Version(reader.read_bits(Version::len())?))
     }
 }
 
+impl Version {
+    fn len() -> usize {
+        3
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
 pub enum PacketType {
     Literal,
-    Operator,
+    Operator(u16),
 }
 
 impl<T: BitReader> ReadFrom<T> for PacketType {
-    fn read_from(reader: &mut T) -> Self {
-        match reader.read_bits(3) {
-            Some(n) => match n {
-                4 => PacketType::Literal,
-                _ => PacketType::Operator,
-            },
-            None => panic!("Corrupt packet"),
+    fn read_from(reader: &mut T) -> Result<Self> {
+        let packet_type = match reader.read_bits(PacketType::len())? {
+            4 => PacketType::Literal,
+            val => PacketType::Operator(val),
+        };
+        Ok(packet_type)
+    }
+}
+
+impl PacketType {
+    fn len() -> usize {
+        3
+    }
+}
+
+#[derive(Debug, Eq)]
+enum PacketContent {
+    Literal(LiteralValue),
+    SubPacketsInBits(Vec<Packet>),
+    NSubPackets(Vec<Packet>),
+}
+
+impl PacketContent {
+    fn len(&self) -> usize {
+        match self {
+            PacketContent::Literal(literal) => literal.len(),
+            PacketContent::SubPacketsInBits(packets) => {
+                LengthType::len()
+                    + SubPacketLengthInBits::len()
+                    + packets.iter().map(Packet::len).sum::<usize>()
+            }
+            PacketContent::NSubPackets(packets) => {
+                LengthType::len()
+                    + NumSubPackets::len()
+                    + packets.iter().map(Packet::len).sum::<usize>()
+            }
         }
+    }
+
+    // It's a hack. Ideally I would like to implement the trait for uniformity.
+    // Or I move this code to Packet
+    fn read_from<T: BitReader>(packet_type: &PacketType, reader: &mut T) -> Result<Self> {
+        if packet_type == &PacketType::Literal {
+            return Ok(PacketContent::Literal(LiteralValue::read_from(reader)?));
+        }
+
+        let length_type = LengthType::read_from(reader)?;
+        let mut sub_packets = Vec::new();
+        match length_type {
+            LengthType::LengthInBits => {
+                let expected_length = SubPacketLengthInBits::read_from(reader)?;
+                let mut length_so_far = 0;
+                while length_so_far < expected_length.0 {
+                    let sub_packet = Packet::read_from(reader)?;
+                    length_so_far += sub_packet.len() as u16;
+                    sub_packets.push(sub_packet);
+                }
+                Ok(PacketContent::SubPacketsInBits(sub_packets))
+            }
+            LengthType::LengthInPackets => {
+                let n = NumSubPackets::read_from(reader)?;
+                for _ in 0..n.0 {
+                    sub_packets.push(Packet::read_from(reader)?);
+                }
+                Ok(PacketContent::NSubPackets(sub_packets))
+            }
+        }
+    }
+}
+
+impl PartialEq for PacketContent {
+    fn eq(&self, other: &Self) -> bool {
+        match (self, other) {
+            (Self::Literal(l0), Self::Literal(r0)) => l0 == r0,
+            (Self::SubPacketsInBits(l0), Self::SubPacketsInBits(r0)) => {
+                l0.len() == r0.len() && l0.iter().zip(r0.iter()).all(|(x, y)| *x == *y)
+            }
+            (Self::NSubPackets(l0), Self::NSubPackets(r0)) => {
+                l0.len() == r0.len() && l0.iter().zip(r0.iter()).all(|(x, y)| *x == *y)
+            }
+            _ => false,
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+pub struct LiteralValue {
+    value: u16,
+    len: usize,
+}
+
+impl<T: BitReader> ReadFrom<T> for LiteralValue {
+    fn read_from(reader: &mut T) -> Result<Self> {
+        let mut value = 0u16;
+        let mut len = 0;
+
+        let mut keep_reading = reader.read_bool()?;
+        while keep_reading {
+            value = (value << 4) + reader.read_bits(4)?;
+            len += 5;
+            keep_reading = reader.read_bool()?;
+        }
+        value = (value << 4) + reader.read_bits(4)?;
+        len += 5;
+
+        Ok(LiteralValue { value, len })
+    }
+}
+
+impl LiteralValue {
+    fn len(&self) -> usize {
+        self.len
+    }
+}
+
+#[derive(Debug)]
+pub struct SubPacketLengthInBits(u16);
+
+impl<T: BitReader> ReadFrom<T> for SubPacketLengthInBits {
+    fn read_from(reader: &mut T) -> Result<Self> {
+        Ok(SubPacketLengthInBits(
+            reader.read_bits(SubPacketLengthInBits::len())?,
+        ))
+    }
+}
+
+impl SubPacketLengthInBits {
+    fn len() -> usize {
+        15
+    }
+}
+
+#[derive(Debug)]
+pub struct NumSubPackets(u16);
+
+impl<T: BitReader> ReadFrom<T> for NumSubPackets {
+    fn read_from(reader: &mut T) -> Result<Self> {
+        Ok(NumSubPackets(reader.read_bits(NumSubPackets::len())?))
+    }
+}
+
+impl NumSubPackets {
+    fn len() -> usize {
+        11
+    }
+}
+
+#[derive(Debug)]
+pub enum LengthType {
+    LengthInBits,
+    LengthInPackets,
+}
+
+impl<T: BitReader> ReadFrom<T> for LengthType {
+    fn read_from(reader: &mut T) -> Result<Self> {
+        match reader.read_bits(LengthType::len())? {
+            0 => Ok(LengthType::LengthInBits),
+            1 => Ok(LengthType::LengthInPackets),
+            _ => unreachable!("Invalid length type"),
+        }
+    }
+}
+
+impl LengthType {
+    fn len() -> usize {
+        1
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::hexreader::HexReader;
+    use anyhow::Result;
+
+    #[test]
+    fn parse_literal() -> Result<()> {
+        let mut reader = HexReader::new("D2FE28");
+
+        let packet = Packet::read_from(&mut reader)?;
+        assert_eq!(packet.header.version, Version(6));
+        assert_eq!(packet.header.packet_type, PacketType::Literal);
+        assert!(matches!(
+            packet.content,
+            PacketContent::Literal(LiteralValue {
+                value: 2021,
+                len: 15
+            })
+        ));
+        assert_eq!(packet.len(), 21);
+        Ok(())
+    }
+
+    #[test]
+    fn parse_bit_wise_subpackets() -> Result<()> {
+        let mut reader = HexReader::new("38006F45291200");
+
+        let packet = Packet::read_from(&mut reader)?;
+
+        assert_eq!(
+            packet,
+            Packet {
+                header: PacketHeader {
+                    version: Version(0b001),
+                    packet_type: PacketType::Operator(0b110),
+                },
+                content: PacketContent::SubPacketsInBits(vec![
+                    Packet {
+                        header: PacketHeader {
+                            version: Version(0b110),
+                            packet_type: PacketType::Literal,
+                        },
+                        content: PacketContent::Literal(LiteralValue { value: 10, len: 5 }),
+                    },
+                    Packet {
+                        header: PacketHeader {
+                            version: Version(0b010),
+                            packet_type: PacketType::Literal,
+                        },
+                        content: PacketContent::Literal(LiteralValue { value: 20, len: 10 }),
+                    }
+                ])
+            }
+        );
+        assert_eq!(packet.len(), 49);
+        Ok(())
+    }
+
+    #[test]
+    fn parse_number_wise_subpackets() -> Result<()> {
+        let mut reader = HexReader::new("EE00D40C823060");
+
+        let packet = Packet::read_from(&mut reader)?;
+
+        assert_eq!(
+            packet,
+            Packet {
+                header: PacketHeader {
+                    version: Version(0b111),
+                    packet_type: PacketType::Operator(0b011),
+                },
+                content: PacketContent::NSubPackets(vec![
+                    Packet {
+                        header: PacketHeader {
+                            version: Version(0b010),
+                            packet_type: PacketType::Literal,
+                        },
+                        content: PacketContent::Literal(LiteralValue { value: 1, len: 5 }),
+                    },
+                    Packet {
+                        header: PacketHeader {
+                            version: Version(0b100),
+                            packet_type: PacketType::Literal,
+                        },
+                        content: PacketContent::Literal(LiteralValue { value: 2, len: 5 }),
+                    },
+                    Packet {
+                        header: PacketHeader {
+                            version: Version(0b001),
+                            packet_type: PacketType::Literal,
+                        },
+                        content: PacketContent::Literal(LiteralValue { value: 3, len: 5 }),
+                    }
+                ])
+            }
+        );
+        assert_eq!(packet.len(), 51);
+        Ok(())
     }
 }
